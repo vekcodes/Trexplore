d0076056887d9e456e5d9a3b431365ddfe9fc42b
vekcodes <shakyab838@gmail.com>
2023-07-27 10:40:34 +0545

    June27

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const playButton = document.getElementById("play-button");
-const pauseButton = document.getElementById("pause-button");
-const video = document.querySelector("video");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the video and buttons
+  const video = document.querySelector("video");
+  const playButton = document.getElementById("play-button");
+  const pauseButton = document.getElementById("pause-button");
 
-playButton.addEventListener("click", () => {
-  video.play();
-});
+  // Add click event listener to the play button
+  playButton.addEventListener("click", function () {
+    // Check if the video is already playing, and prevent playing it again
+    if (!video.paused) {
+      return;
+    }
 
-pauseButton.addEventListener("click", () => {
-  video.pause();
+    // Play the video
+    video.play();
+
+    // Hide the play button and show the pause button
+    playButton.style.display = "none";
+    pauseButton.style.display = "inline-block";
+  });
+
+  // Add click event listener to the pause button
+  pauseButton.addEventListener("click", function () {
+    // Check if the video is already paused, and prevent pausing it again
+    if (video.paused) {
+      return;
+    }
+
+    // Pause the video
+    video.pause();
+
+    // Hide the pause button and show the play button
+    pauseButton.style.display = "none";
+    playButton.style.display = "inline-block";
+  });
 });
